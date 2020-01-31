2bd7a9781bcc3caa90f315838973df384464e677
epozen <epozen@DESKTOP-LOPSEJG>
2020-01-31 13:35:53 +0900

    init commit

---
+++ src/main/java/com/epozen/epozenMall/EpozenMallApplication.java
@@ -0,0 +1,13 @@
+package com.epozen.epozenMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpozenMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpozenMallApplication.class, args);
+	}
+
+}
