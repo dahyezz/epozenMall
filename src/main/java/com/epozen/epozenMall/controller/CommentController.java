751ba0c23451e582b54378123dd69db625e662b2
EPOZEN <EPOZEN@DESKTOP-CLTG5MC>
2020-02-07 17:25:07 +0900

    댓글페이지

--- src/main/java/com/epozen/epozenMall/controller/CommentController.java
+++ src/main/java/com/epozen/epozenMall/controller/CommentController.java
@@ -1,14 +1,40 @@
 package com.epozen.epozenMall.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.epozen.epozenMall.service.face.CommentService;
+import com.epozen.epozenMall.vo.ShopCommentVO;
 
-@Controller
+@RestController
 public class CommentController {
 	
 	@Autowired
 	CommentService commentService;
+	
+	@PostMapping("/comment")
+	public void insert(@ModelAttribute ShopCommentVO shopCommentVO, HttpSession session) {
+		String userId = (String) session.getAttribute("userId");
+		shopCommentVO.setUserId(userId);
+		commentService.insertComment(shopCommentVO);
+		
+	}
+	
+	@GetMapping("/comment")
+	public List<ShopCommentVO> commentList(@RequestParam int proNo){
+		List<ShopCommentVO> list = commentService.selectComments(proNo);
+		
+		return list;
+	}
 
 }
--- src/main/java/com/epozen/epozenMall/dao/CommentMapper.java
+++ src/main/java/com/epozen/epozenMall/dao/CommentMapper.java
@@ -1,5 +1,13 @@
 package com.epozen.epozenMall.dao;
 
+import java.util.List;
+
+import com.epozen.epozenMall.vo.ShopCommentVO;
+
 public interface CommentMapper {
+
+	List<ShopCommentVO> selectComments(int proNo);
+
+	void insertComment(ShopCommentVO shopCommentVO);
 
 }
