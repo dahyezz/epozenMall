e34d63d07c9ea98d81f4652847653cd9618bfc59
epozen <epozen@DESKTOP-LOPSEJG>
2020-02-12 09:55:58 +0900

    productServiceImpl.java 주석
     - 구매하기 버튼 동작 시 실행되는 코드 주석 추가

--- src/main/java/com/epozen/epozenMall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/epozen/epozenMall/service/impl/ProductServiceImpl.java
@@ -74,10 +74,9 @@ public class ProductServiceImpl implements ProductService{
 	@Override
 	public void buyOrderde(Map<String, Object> map) {
 
+		// 마지막으로 생성된 orderNo 조회
 		int orderNo = productMapper.selectTopOrderNo();
-		UserOrderVO userOrderVO = new UserOrderVO();
-		userOrderVO.setUserId(map.get("userId").toString());
-		
+			
 		ShopOrderdeVO shopOrderdeVO = new ShopOrderdeVO();
 		shopOrderdeVO.setOrderNo(orderNo);
 		
@@ -89,6 +88,13 @@ public class ProductServiceImpl implements ProductService{
 		String[] priceList = priceStr.split(",");
 		int[] priList = new int[priceList.length];
 		
+		// 장바구니에서 삭제하기 위한 vo
+		UserOrderVO userOrderVO = new UserOrderVO();
+		userOrderVO.setUserId(map.get("userId").toString());
+		
+		
+		// ------------------------------------------------------
+		//   shop_orderde 테이블에 insert, shop_cart delete 부분
 		for(int i=0; i<proList.length; i++) {
 			proList[i] = Integer.parseInt(productList[i].toString());
 			priList[i] = Integer.parseInt(priceList[i].toString());
@@ -98,11 +104,21 @@ public class ProductServiceImpl implements ProductService{
 			shopOrderdeVO.setProNo(proList[i]);
 			productMapper.insertOrderde(shopOrderdeVO);
 			
+			// ------------------------------------------------------
+			// shop_cart에서 구매완료된 상품 삭제하는 부분
+			
+			// 파라미터 : userOrderVO 이용 (userId, proNo)
+			// 		userId - map 활용
+			//		proNo - 구매완료한 상품들의 proNo를 setting 해줌
 			userOrderVO.setProNo(proList[i]);
+
+			//	cartNo - 장바구니 삭제 시 필요한 파라미터를 조회해옴
 			int cartNo = productMapper.selectCartNoByUserInfo(userOrderVO);
+			
 			cartMapper.deleteCartByCartNo(cartNo);
+			// ------------------------------------------------------
 		}
-		
+		// ------------------------------------------------------
 		
 	}
 	
